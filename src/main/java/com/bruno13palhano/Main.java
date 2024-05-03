0e3c06cfb7e23a8ac1d1b0b9c5814f47cb868412
bruno13palhano <bruno13palhano@gmail.com>
2024-05-03 16:14:14 -0300

    Implement mergeSort algorithm

--- src/main/java/com/bruno13palhano/Main.java
+++ src/main/java/com/bruno13palhano/Main.java
@@ -4,11 +4,17 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {9,1,2,5,3,7,14,13,45,43,11,23,45,3};
+        int[] arr = {100,90,80,7,8,5,4,1,2,3};
 
         System.out.println(Arrays.toString(arr));
         insertionSort(arr, arr.length);
         System.out.println(Arrays.toString(arr));
+
+        arr = new int[]{100,90,80,7,8,5,4,1,2,3};
+        System.out.println(Arrays.toString(arr));
+        mergeSort(arr,  0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+
     }
 
     static void insertionSort(int[] array, int n) {
@@ -22,5 +28,39 @@ public class Main {
             }
             array[j+1] = key;
         }
+    }
+
+    static void mergeSort(int[] array, int start, int end) {
+        if (start < end) {
+            int middle = (start+end) / 2;
+            mergeSort(array, start, middle);
+            mergeSort(array, middle + 1, end);
+            merge(array, start, middle, end);
+        }
+    }
+
+    static void merge(int[] array, int start, int middle, int end) {
+        int leftSize = middle - start + 1;
+        int rightSize = end - middle;
+        int[] leftArray = new int[leftSize];
+        int[] rightArray = new int[rightSize];
+
+        int i, j, k = start;
+
+        for (i = 0; i < leftSize; i++) leftArray[i] = array[start + i];
+
+        for (j = 0; j < rightSize; j++) rightArray[j] = array[middle + j + 1];
+
+        i = 0;
+        j = 0;
+
+        while (i < leftSize && j < rightSize) {
+            if (leftArray[i] <= rightArray[j]) array[k++] = leftArray[i++];
+            else array[k++] = rightArray[j++];
+        }
+
+        while (i < leftSize) array[k++] = leftArray[i++];
+
+        while (j < rightSize) array[k++] = rightArray[j++];
     }
 }
